8dada5c339a3165c6c73d6010042a8e700874d10
Simon King <simon.king65@gmail.com>
2014-11-08 06:53:27 +0000

    adding size

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,7 +29,7 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
+    debugger
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
