44f9785623c5c39506102dd61efe30c365f9e2be
Simon King <simon.king65@gmail.com>
2014-11-26 07:52:54 +0000

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-KingClothing::application.configure do
+Kingclothing::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
