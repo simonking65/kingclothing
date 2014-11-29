caddd4b2d8cca7cdea7e403aac6c04de3584178e
Simon King <simon.king65@gmail.com>
2014-11-29 23:06:18 +0000

    heroku logging

--- app/models/order.rb
+++ app/models/order.rb
@@ -42,6 +42,8 @@ attr_accessor :return_url, :cancel_url, :payment_method
       self.payment_id = @payment.id
       self.state      = @payment.state
       save
+      puts "@payment save just done"
+      puts @payment.inspect
     else
       errors.add :payment_method, @payment.error["message"] if @payment.error
       raise ActiveRecord::Rollback, "Can't place the order"
