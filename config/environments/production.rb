fa75a2d240f6a6b12fdb20329164939168a10b09
Simon King <simon.king65@gmail.com>
2014-12-07 22:45:23 +0000

    working on email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,6 @@ Kingclothing::Application.configure do
     config.action_mailer.smtp_settings = { 
     :address => "smtp.gmail.com", 
     :port => 587,
-    :domain => "gmail.com",
     :authentication => :plain, 
     :user_name => "simon.king65@gmail.com", 
     :password => "teddy1067",
