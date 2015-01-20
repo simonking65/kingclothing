252ceed3ffbcc0695dcd949cd3061b53cd837fe2
Simon King <simon.king65@gmail.com>
2015-01-20 21:31:48 +0000

    fixed email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,6 @@ Kingclothing::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = { 
-    :tls => true,
     :address => "smtp.gmail.com", 
     :port => 587,
     :authentication => :plain, 
