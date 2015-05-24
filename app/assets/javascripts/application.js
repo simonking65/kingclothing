e4b84be026ec43d9250fe41dc6937014f243e309
Simon King <simon.king65@gmail.com>
2015-05-24 16:40:49 +0100

    sorting out cart and menus

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,4 @@
 //= require jquery
 //= require jquery-ui/effect-blind
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,6 +28,7 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
+    
     @line_item = @cart.add_product(product.id)
     @line_item.size = params[:item][:sizze]
     #debugger
