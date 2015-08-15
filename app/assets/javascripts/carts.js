ffbef3e735bac71ffb17276903b0a331c3f5bdec
Simon King <simon.king65@gmail.com>
2015-08-15 21:15:29 +0100

    new basket

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -1,28 +1,28 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
-$(function(){
+// $(function(){
 
-	//The floaty widget that appears on help/cpages
-	var myWidget = $("#side");
-	if (myWidget.length > 0) {
-		$(window).scroll(function() {
-			var scrollPos = $(this).scrollTop(),
-				bodyHeight = $("body").height(),
-				windowHeight = $(this).height(),
-				footerheight = 10;
+// 	//The floaty widget that appears on help/cpages
+// 	var myWidget = $("#side");
+// 	if (myWidget.length > 0) {
+// 		$(window).scroll(function() {
+// 			var scrollPos = $(this).scrollTop(),
+// 				bodyHeight = $("body").height(),
+// 				windowHeight = $(this).height(),
+// 				footerheight = 10;
 
-			if (scrollPos+windowHeight > bodyHeight-footerheight) {
-				//do nothing
-		    } else if ((bodyHeight > windowHeight*2) && scrollPos > 170) {
-		        myWidget.css("top", scrollPos-50);
-		    } else {
-		        myWidget.css("top", "100px");
-		    }
-		});
+// 			if (scrollPos+windowHeight > bodyHeight-footerheight) {
+// 				//do nothing
+// 		    } else if ((bodyHeight > windowHeight*2) && scrollPos > 170) {
+// 		        myWidget.css("top", scrollPos-50);
+// 		    } else {
+// 		        myWidget.css("top", "100px");
+// 		    }
+// 		});
 		
-	}
+// 	}
 	
 	
 
-	});
+// 	});
